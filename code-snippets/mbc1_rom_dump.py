4d63bd10cf1abf4abdd21669b9f95d1de77477cb
Joonas Javanainen <joonas.javanainen@gmail.com>
2024-01-07 20:47:02 +0200

    Clarify registers in rom dump pseudocode

--- code-snippets/mbc1_rom_dump.py
+++ code-snippets/mbc1_rom_dump.py
@@ -1,11 +1,14 @@
-write_byte(0x6000, 0x01)
+BANK1 = 0x2000
+BANK2 = 0x4000
+MODE = 0x6000
+write_byte(MODE, 0x01)
 for bank in range(0, num_banks):
-    write_byte(0x2000, bank)
+    write_byte(BANK1, bank)
     if is_multicart:
-        write_byte(0x4000, bank >> 4)
+        write_byte(BANK2, bank >> 4)
         bank_start = 0x4000 if bank & 0x0f else 0x0000
     else:
-        write_byte(0x4000, bank >> 5)
+        write_byte(BANK2, bank >> 5)
         bank_start = 0x4000 if bank & 0x1f else 0x0000
     for addr in range(bank_start, bank_start + 0x4000):
         buf += read_byte(addr)
--- code-snippets/mbc2_rom_dump.py
+++ code-snippets/mbc2_rom_dump.py
@@ -1,5 +1,6 @@
+ROMB = 0x2100
 for bank in range(0, num_banks):
-    write_byte(0x2100, bank)
+    write_byte(ROMB, bank)
     bank_start = 0x4000 if bank > 0 else 0x0000
     for addr in range(bank_start, bank_start + 0x4000):
         buf += read_byte(addr)
